31f4c8ede5066d68525d4373695c035baff63476
Sam Davies <sdavies@shinobicontrols.com>
2015-01-15 22:57:00 +0000

    Completed a demo of argo
    Including adding a sample JSON file

--- JSONShootOut.playground/section-1.swift
+++ JSONShootOut.playground/section-1.swift
@@ -1,3 +1,73 @@
 import Cocoa
+import Foundation
 import Argo
+import Runes
+
+/* Import the JSON from a file */
+let jsonURL = NSBundle.mainBundle().pathForResource("sams_repos", ofType: "json")
+let rawJSON = NSData(contentsOfFile: jsonURL!)
+let json: AnyObject? = NSJSONSerialization.JSONObjectWithData(rawJSON!,
+                                            options: .allZeros, error: nil)
+
+//-------------//
+// MARK:- ARGO
+//-------------//
+
+struct Repo {
+  let id: Int
+  let name: String
+  let desc: String?
+  let url: NSURL
+  let homepage: NSURL?
+  let fork: Bool
+}
+
+extension NSURL: JSONDecodable {
+  public class func decode(j: JSONValue) -> NSURL? {
+    switch j {
+    case .JSONString(let s):
+      return NSURL(string: s)
+    default:
+      return nil
+    }
+  }
+}
+
+extension Repo: JSONDecodable {
+  static func create(id: Int)(name: String)(desc: String?)
+                    (url: NSURL)(homepage: NSURL?)(fork: Bool) -> Repo {
+    return Repo(id: id, name: name, desc: desc,
+                url: url, homepage: homepage, fork: fork)
+  }
+  
+  static func decode(j: JSONValue) -> Repo? {
+    return Repo.create
+      <^> j <|  "id"
+      <*> j <|  "name"
+      <*> j <|? "description"
+      <*> j <|  "url"
+      <*> j <|? "homepage"
+      <*> j <|  "fork"
+  }
+}
+
+extension Repo: Printable {
+  var description : String {
+    return "\(name) (\(id)) {\(desc) :: \(homepage)}"
+  }
+}
+
+if let j: AnyObject = json {
+  if let value = JSONValue.parse <^> j {
+    switch value {
+    case .JSONArray(let a):
+      let repos = a.map(Repo.decode)
+      println(repos.first!!.description)
+    default:
+      println("Not an array")
+    }
+  }
+}
+
+
 
