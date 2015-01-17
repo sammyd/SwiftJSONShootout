43f0d2badcabe86b203e6c83d6efe42a418f148e
Sam Davies <sdavies@shinobicontrols.com>
2015-01-17 11:29:53 +0000

    Added a section using valueForKey to parse the JSON

--- JSONShootOut.playground/section-1.swift
+++ JSONShootOut.playground/section-1.swift
@@ -22,11 +22,44 @@ struct Repo {
   let fork: Bool
 }
 
+extension Repo: Printable {
+  var description : String {
+    return "\(name) (\(id)) {\(desc) :: \(homepage)}"
+  }
+}
+
 
 //--------------------//
 // MARK:- valueForKey
 //--------------------//
+var repos = [Repo]()
 
+if let json : AnyObject = json {
+  if let array = json as? NSArray {
+    for jsonItem in array as [AnyObject] {
+      if let id = jsonItem.valueForKey("id") as? Int {
+        if let name = jsonItem.valueForKey("name") as? String {
+          if let url_string = jsonItem.valueForKey("url") as? String {
+            if let fork = jsonItem.valueForKey("fork") as? Bool {
+              if let url = NSURL(string: url_string) {
+                var description = jsonItem.valueForKey("description") as? String
+                var homepage: NSURL? = .None
+                if let homepage_string = jsonItem.valueForKey("homepage") as? String {
+                  homepage = NSURL(string: homepage_string)
+                }
+                let repo = Repo(id: id, name: name, desc: description, url: url,
+                                homepage: homepage, fork: fork)
+                repos += [repo]
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+println(repos)
 
 
 //----------------------//
