3d90bd38be6be0bb85c82213f61d374bd1f4ee26
Sam Davies <sdavies@shinobicontrols.com>
2015-01-23 19:10:28 +0000

    Begins work on a swiftz implementation
    Doesn't actually work yet. Documentation is pretentious, where it exists
    Will return to this with access to the internet.

--- JSONShootOut_Swiftz.playground/section-1.swift
+++ JSONShootOut_Swiftz.playground/section-1.swift
@@ -2,7 +2,7 @@
 
 
 import Cocoa
-import SwiftyJSON
+import Swiftz
 
 /* Import the JSON from a file */
 let jsonURL = NSBundle.mainBundle().pathForResource("sams_repos", ofType: "json")
@@ -29,3 +29,37 @@ extension Repo: Printable {
 }
 
 
+//-------------//
+// MARK:- Swiftz
+//-------------//
+
+extension Repo: JSONDecode {
+  typealias J = Repo
+  
+  static func create(id: Int)(name: String)(desc: String?)
+    (url: NSURL)(homepage: NSURL?)(fork: Bool) -> Repo {
+      return Repo(id: id, name: name, desc: desc,
+        url: url, homepage: homepage, fork: fork)
+  }
+  
+  static func fromJSON(x: JSONValue) -> Repo? {
+    var id: Int?
+    var name: String?
+    var desc: String?
+    var url: NSURL?
+    var homepage: NSURL?
+    var fork: Bool?
+    switch x {
+      case let .JSONObject(d):
+        id = d["id"]   >>- JInt.fromJSON
+        name = d["name"]    >>- JString.fromJSON
+        desc = d["description"] >>- JString.fromJSON
+        url = d["url"]  >>- JString.fromJSON
+        homepage = d["homepage"] >>- JString.fromJSON
+        fork = d["fork"] >>- JBool.fromJSON
+        return (Repo.create <^> id <*> name <*> desc <*> url <*> homepage <*> fork)
+      default:
+        return .None
+    }
+  }
+}
